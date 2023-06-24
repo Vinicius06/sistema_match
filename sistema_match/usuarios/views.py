5a8d8d275b8750fac6e371057067d23499579e19
Vinicius <vinicius.camargo.bandeira@gmail.com>
2023-06-24 19:20:58 -0300

    terminei a parte do register, indo pra parte de logar

--- sistema_match/usuarios/views.py
+++ sistema_match/usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == "GET":
@@ -8,7 +9,14 @@ def cadastro(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
-        return HttpResponse(username)
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Já existe um usuario com esse username')
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        return HttpResponse('Usuário cadastrado com sucesso!')
 
 
 
