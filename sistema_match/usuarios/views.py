bdf7a4246375a7393f3743266ac13a0cca12c48d
Vinicius <vinicius.camargo.bandeira@gmail.com>
2023-06-24 19:39:54 -0300

    login e cadastro finalizado

--- sistema_match/usuarios/views.py
+++ sistema_match/usuarios/views.py
@@ -1,6 +1,10 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
+
 
 def cadastro(request):
     if request.method == "GET":
@@ -21,4 +25,20 @@ def cadastro(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+        user = authenticate(username=username, password=senha)
+        
+        if user: 
+            login_django(request, user)
+            return HttpResponse('autenticado')
+        else: 
+            return HttpResponse('Email ou senha invalidos')
+        
+@login_required(login_url="/auth/login/")
+def plataforma(request):
+        return HttpResponse('Plataforma')
